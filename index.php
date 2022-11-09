d8a0a9b422bbd9e75701ae3a6f2c1eccec7cc4cf
Brandoneius <brandmthomas@yahoo.com>
2022-11-09 14:33:59 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -5,34 +5,6 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-// create connection
-$conn = new mysqli($dbServerName, $dbUsername, $dbPassword, $dbName);
-
-// check connection
-if ($conn->connect_error) {
-	die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully<br>";
-
-
-/*
- * get data from remote database table
- */
-$sql = "SELECT id, name FROM dmu WHERE 1";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        //echo "id: " . $row["id"] . " SLOW <br>";
-    }
-
-
-} else {
-    echo "0 results";
-}
-
-
 function pack_create(){
     //A DMU pack is 1 rare (13.25% chance to be mythic) 3 uncommon, 10 common, and 1 basic land
     //Going to set up an array with 1-15 as the indices each being the card to be taken track of
@@ -43,18 +15,21 @@ function pack_create(){
 
     );
 
+
+    $dbServerName = "162.241.30.19";
+    $dbUsername = "insulin8_card_fetch";
+    $dbPassword = "?Sop_JI,Y[*&";
+    $dbName = "insulin8_dmu_cards";
+    $conn = new mysqli($dbServerName, $dbUsername, $dbPassword, $dbName);
     $sql = "SELECT id, name FROM dmu WHERE rarity = 'uncommon';";
     $result = $conn->query($sql);
 
-    if($result->num_rows > 0){
-        foreach($result as $card){
-            var_dump($card) . "<br>";
-        }
-    }
 
+
+    $conn->close();
     return $pack;
     }
 
 echo pack_create();
-$conn->close();
+
 ?>
